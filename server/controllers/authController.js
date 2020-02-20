5a68ccdcf1aa69e4be016ff1454fe2e14b231286
Anthony Coats <acoats93@gmail.com>
2020-02-20 01:33:50 -0600

    resubmit

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -3,16 +3,27 @@ const users = require('../models/users');
 let id = 1;
 
 module.exports = {
-    register:(req,res) =>{
-        const {session} = req.session.user;
+    // register:(req,res) =>{
+    //     const {session} = req;
+    //     const {username, password} = req.body;
+
+    //     users.push({id, username, password});
+    //     id++;
+
+    //     session.user.username = username;
+    //     res.status(200).json(users);
+    // },
+    register: (req, res) => {
+        const {session} = req;
         const {username, password} = req.body;
-
-        users.push({id, username, password});
+    
+        users.push({ id, username, password });
         id++;
-
+    
         session.user.username = username;
-        res.status(200).json(users);
-    },
+    
+        res.status(200).send(session.user);
+      },
     login:(req,res) =>{
         const {session} = req;
         const {username, password} = req.body;
